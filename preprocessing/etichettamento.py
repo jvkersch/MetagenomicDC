f41583745a545c6c50eadea6001e7fd9bc3462e7
Joris Vankerschaver <jvankerschaver@enthought.com>
2019-06-27 17:44:38 -0500

    MAINT: Use dict instead of quadratic join

--- preprocessing/etichettamento.py
+++ preprocessing/etichettamento.py
@@ -17,6 +17,8 @@ outputFileFamily.write(matrix[0])
 outputFileOrder.write(matrix[0])
 outputFileClass.write(matrix[0])
 matrix=matrix[1:]
+
+taxonomy_data = {}
 for seq in records:
 	elements=seq.split(" ")
 	sequ=elements[0][1:]
@@ -24,13 +26,15 @@ for seq in records:
 	ordine=elements[2]
 	famiglia=elements[3]
 	genere=elements[4]
-	matrix = open(sys.argv[1], "r")
-	for line in matrix:
-		sequ2num=line.split(",")[0];
-		#print (sequ2num)
-		sequ2=sequ2num.split("-")[0];
-		if (sequ == sequ2):
-			outputFileGenus.write(line.split("\n")[0]+","+genere)
-			outputFileFamily.write(line.split("\n")[0]+","+famiglia+"\n")
-			outputFileOrder.write(line.split("\n")[0]+","+ordine+"\n")
-			outputFileClass.write(line.split("\n")[0]+","+classe+"\n")
+        taxonomy_data[sequ] = (classe, ordine, famiglia, genere)
+        
+
+for line in matrix:
+	sequ2num=line.split(",")[0];
+	sequ2=sequ2num.split("-")[0];
+        classe, ordine, famiglia, genere = taxonomy_data[sequ2]
+        
+        outputFileGenus.write(line.split("\n")[0]+","+genere)
+	outputFileFamily.write(line.split("\n")[0]+","+famiglia+"\n")
+	outputFileOrder.write(line.split("\n")[0]+","+ordine+"\n")
+	outputFileClass.write(line.split("\n")[0]+","+classe+"\n")
